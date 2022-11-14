0c9ea333add6cb662004d7e20e055c187d93f828
Eemil V <eemilv11@gmail.com>
2022-11-14 20:52:59 +0200

    Arrow and the speech bubble type arrow svg icons for use in the FAQ page

---
+++ frontend/src/components/FaqAccordion/TextSorting.tsx
@@ -0,0 +1,8 @@
+
+export function TextSorting() {
+    return (
+    <svg width="28" height="22" viewBox="0 0 28 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14 0L27.8564 21.75H0.143594L14 0Z" fill="#e5e7eb"/>
+    </svg>
+    )
+}
