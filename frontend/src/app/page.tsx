ed04edefd7918d22bb45a7e856f9adf421c63c84
Eemil V <eemilv11@gmail.com>
2022-11-14 20:52:21 +0200

    Items have link attribute, so we can use that.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import { GridItems, HomeText } from "@/services";
 const Home = async () => {
   const [homeText, gridItems] = await Promise.all([HomeText(), GridItems()]);
 
+
   return (
     <div>
       <Heading>{homeText}</Heading>
@@ -13,7 +14,7 @@ const Home = async () => {
         <Grid>
           {gridItems.data.length > 0 &&
             gridItems.data.map((item) => (
-              <GridItem key={item.id} href={"/exhibitors"}>
+              <GridItem key={item.id} href={item.attributes.link}>
                 {item.attributes.icon && (
                   <GridIcon icon={item.attributes.icon} />
                 )}
