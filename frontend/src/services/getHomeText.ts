03ba669a6fd72636ae3dd1fe0da8411301cc287e
Petr Halinen <petr.halinen@tuni.fi>
2022-11-19 17:11:18 +0200

    Modify Strapi home content-type, add language var.

--- frontend/src/services/getHomeText.ts
+++
@@ -1,7 +0,0 @@
-import { client } from "@/support/client";
-
-export async function getHomeText() {
-  return client
-    .get("/api/home")
-    .then((response) => response.data.data.attributes.top_text);
-}
