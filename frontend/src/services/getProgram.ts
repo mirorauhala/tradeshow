4db0d4fe27a9a8abd8175db9243691939e1bf260
Eemil V <eemilv11@gmail.com>
2022-11-19 12:12:20 +0200

    Fetching function for the time, also removes seconds/milliseconds and sorts them by the time

---
+++ frontend/src/services/getProgram.ts
@@ -0,0 +1,25 @@
+import { client } from "@/support/client";
+
+
+interface ProgramResponse{
+    id?: number;
+    attributes: { start_time: string; name: string; };
+}
+export interface ProgramInterface {
+    id?: number;
+    startTime: string;
+    name: string;
+}
+
+
+export async function getProgram() {
+    const program = await client.get("/api/programs?populate=*").then((response) => response.data);
+    return program.data.map((program: ProgramResponse) => {
+        return {
+            id: program.id,
+            name: program.attributes.name,
+            startTime: program.attributes.start_time.substring(0, program.attributes.start_time.indexOf(':', program.attributes.start_time.indexOf(':') + 1)),
+        }})
+        .sort((a: ProgramInterface, b: ProgramInterface) => a.startTime.localeCompare(b.startTime));
+
+}
